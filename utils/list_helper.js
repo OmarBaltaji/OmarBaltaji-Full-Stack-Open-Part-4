d52150e7a0c48e748917e63a63b1573fd53c52d0
Omar Baltaji <omr.baltaji@gmail.com>
2023-06-17 02:46:28 +0300

    step5 - modifying the most likes and blogs algorithm

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,27 +3,18 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  const reducer = (sum, item) => {
-    return sum + item.likes
-  }
+  const reducer = (sum, item) =>  sum + item.likes;
 
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 }
 
 const favoriteBlog = (blogs) => {
   const reducer = (item1, item2) => {
-    if(item1.likes > item2.likes) {
-      return {
-        title: item1.title,
-        author: item1.author,
-        likes: item1.likes
-      }
-    } else {
-      return {
-        title: item2.title,
-        author: item2.author,
-        likes: item2.likes
-      }
+    itemWithMoreLikes = item1.likes > item2.likes ? item1 : item2;
+    return {
+      title: itemWithMoreLikes.title,
+      author: itemWithMoreLikes.author,
+      likes: itemWithMoreLikes.likes
     }
   }
 
@@ -31,69 +22,39 @@ const favoriteBlog = (blogs) => {
 }
 
 const mostBlogs = (blogs) => {
-  uniqueAuthors = [];
-  authorsAndBlogsArray = [];
-  blogs.forEach(blog => {
-    if(uniqueAuthors.includes(blog.author)) {
-      authorsAndBlogsArray = authorsAndBlogsArray.map(item => { 
-        if(item.author === blog.author) {
-          item.blogs += 1;
-          return item;
-        }
-      });
-    } else {
-      authorsAndBlogsObject = {
-        author: blog.author,
-        blogs: 1
-      };
-      authorsAndBlogsArray.push(authorsAndBlogsObject);
-      uniqueAuthors.push(blog.author);
-    }
-  })
-
-  mostBlogsAuthor = authorsAndBlogsArray.reduce((item1, item2) => {
-    if(item1.blogs > item2.blogs) {
-      return item1;
-    } else {
-      return item2;
-    }
-  }, -Infinity)
-
+  const mostBlogsAuthor = getAuthorWithMoreProperty(blogs, 'blogs');
   return blogs.length === 0 ? {} : mostBlogsAuthor
 }
 
 const mostLikes = (blogs) => {
-  uniqueAuthors = [];
-  authorsAndBlogsArray = [];
-  blogs.forEach(blog => {
-    if(uniqueAuthors.includes(blog.author)) {
-      authorsAndBlogsArray = authorsAndBlogsArray.map(item => { 
-        if(item.author === blog.author) {
-          item.likes += blog.likes;
-          return item;
-        }
-      });
-    } else {
-      authorsAndBlogsObject = {
-        author: blog.author,
-        likes: blog.likes
-      };
-      authorsAndBlogsArray.push(authorsAndBlogsObject);
-      uniqueAuthors.push(blog.author);
-    }
-  })
-
-  console.log(authorsAndBlogsArray);
-
-  mostBlogsAuthor = authorsAndBlogsArray.reduce((item1, item2) => {
-    if(item1.likes > item2.likes) {
-      return item1;
-    } else {
-      return item2;
-    }
-  }, -Infinity)
-
-  return blogs.length === 0 ? {} : mostBlogsAuthor
+  const mostLikesAuthor = getAuthorWithMoreProperty(blogs, 'likes');
+  return blogs.length === 0 ? {} : mostLikesAuthor
 }
+
+const getAuthorWithMoreProperty = (blogs, property) => {
+  const propertyCountByAuthor = {};
+  blogs.forEach(blog => {
+    const author = blog.author;
+    if (propertyCountByAuthor[author]) {
+      propertyCountByAuthor[author] += (property === 'likes' ? blog.likes : 1);
+    } else {
+      propertyCountByAuthor[author] = property === 'likes' ? blog.likes : 1;
+    }
+  });
+ 
+  let maxProperty = 0;
+  let authorWithMostProperty = '';
+  for (const author in propertyCountByAuthor) {
+    if (propertyCountByAuthor[author] > maxProperty) {
+      maxProperty = propertyCountByAuthor[author];
+      authorWithMostProperty = author;
+    }
+  }
+
+  return {
+    author: authorWithMostProperty,
+    [property]: maxProperty,
+  }
+} 
 
 module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
